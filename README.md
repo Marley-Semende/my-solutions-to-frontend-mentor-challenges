d2fe4f3166ff3a3ca33cf540de3dc315ae5c8ee0
Marley-Semende <marley.semende@gmail.com>
2023-12-22 05:00:44 +0100

    Add initial files for javascript faq accordion

---
+++ README.md
@@ -0,0 +1,56 @@
+# Frontend Mentor - FAQ accordion solution
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+This my solution to the [FAQ accordion challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-wyfFdeBwBz).
+
+### The challenge
+
+Users should be able to:
+
+- Hide/Show the answer to a question when the question is clicked
+- Navigate the questions and hide/show answers using keyboard navigation alone
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [My Solution Link]()
+- Live Site URL: [Live Site Link]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Variables
+- Mobile-first workflow
+- Javascript DOM
+
+### What I learned
+
+## Author
+
+- Name - [Marley]
+- Frontend Mentor - [@Marley-Semende](https://www.frontendmentor.io/profile/Marley-Semende)
+- LinkedIn - [Marley](https://www.linkedin.com/in/marley-semende-web-dev/)
---
+++ script.js
@@ -0,0 +1,38 @@
+const iconOne = document.querySelector(".plus-icon-one");
+const answerElementOne = document.getElementById("answer-1");
+const iconTwo = document.querySelector(".plus-icon-two");
+const iconThree = document.querySelector(".plus-icon-three");
+const iconFour = document.querySelector(".plus-icon-four");
+const minusButton = "assets/images/icon-minus.svg";
+const answerElement = document.querySelectorAll(".other-answers");
+
+iconOne.addEventListener("click", (e) => {
+  iconOne.src = minusButton;
+  iconOne.style.cursor = "pointer";
+  // answerElementOne.innerHTML = ``;
+  // answerElementOne.style.color = "";
+});
+
+iconTwo.addEventListener("click", (e) => {
+  iconTwo.src = minusButton;
+  iconTwo.style.cursor = "pointer";
+  answerElement[0].innerHTML =
+    "Yes, Frontend Mentor offers both free and premium coding challenges, with the freeoption providing access to a range of projects suitable for all skill levels.";
+  answerElement[0].style.color = "hsl(292, 16%, 49%)";
+});
+
+iconThree.addEventListener("click", (e) => {
+  iconThree.src = minusButton;
+  iconThree.style.cursor = "pointer";
+  answerElement[1].innerHTML =
+    "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!</p>";
+  answerElement[1].style.color = "hsl(292, 16%, 49%)";
+});
+
+iconFour.addEventListener("click", (e) => {
+  iconFour.src = minusButton;
+  iconFour.style.cursor = "pointer";
+  answerElement[2].innerHTML =
+    "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.";
+  answerElement[2].style.color = "hsl(292, 16%, 49%)";
+});
