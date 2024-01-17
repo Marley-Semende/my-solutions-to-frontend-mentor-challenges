786b2948e5e6cecb07055203de00c003aed69e67
Marley-Semende <marley.semende@gmail.com>
2024-01-17 09:52:49 +0100

    add initial commit for news-homepage

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navBar = document.querySelector(".nav-bar");
+const lineOne = document.querySelector(".hamburger-1");
+const lineTwo = document.querySelector(".hamburger-2");
+const lineThree = document.querySelector(".hamburger-3");
+let isMenuOpen = false;
+
+hamburgerMenu.addEventListener("click", (e) => {
+  isMenuOpen = !isMenuOpen;
+  if (isMenuOpen) {
+    lineOne.style.transform = "rotate(-45deg) translate(-5px, 6px)";
+    lineTwo.style.opacity = 0;
+    lineThree.style.transform = "rotate(45deg) translate(-5px, -6px)";
+    navBar.classList.remove("hidden");
+  } else {
+    lineOne.style.transform = "";
+    lineTwo.style.opacity = 1;
+    lineThree.style.transform = "";
+    navBar.classList.add("hidden");
+  }
+});
